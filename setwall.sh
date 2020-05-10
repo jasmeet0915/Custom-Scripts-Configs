953b0fc4408e064a0bd549fca30050869c1d4249
Jasmeet Singh <jasmeet0915@gmail.com>
2020-05-10 21:00:42 +0530

    notification & themes added

--- setwall.sh
+++ setwall.sh
@@ -25,7 +25,7 @@ wall="$XDG_DATA_HOME/wall"
 # the links till no more links are obtained & original file is reached
 
 [ -f "$1" ] && ln -sf "$(readlink -f "$1")" "$wall" && notify-send -i "$wall" -t 3000 "Wallpaper Changed"
-
+test "$1"="themes" && ls $XDG_DATA_HOME/wallpapers
 
 
 # set the wallpaper using xwallpaper
